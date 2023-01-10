8768ec32b68964c7645eb113cab687fe5cd750a9
Asher Brooke <asherbrooke9@gmail.com>
2023-01-10 19:58:44 +0700

    no message

---
+++ CRUD/CRUD/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using CRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        static public List<Product> Products = new List<Product>{
+
+            new Product { Id = 1, Name = "CPU", Description = "CPU AMD 5", Price = 5000, DateTime = new DateTime (1999, 09, 17) },
+
+            new Product { Id = 2, Name = "CPU", Description = "CPU AMD 5", Price = 5000, DateTime = new DateTime (1999, 09, 17) },
+
+            new Product { Id = 3, Name = "CPU", Description = "CPU AMD 5", Price = 5000, DateTime = new DateTime (1999, 09, 17) },
+
+            new Product { Id = 4, Name = "CPU", Description = "CPU AMD 5", Price = 5000, DateTime = new DateTime (1999, 09, 17) },
+
+            new Product { Id = 5, Name = "CPU", Description = "CPU AMD 5", Price = 5000, DateTime = new DateTime (1999, 09, 17) },
+
+        };
+
+        [HttpGet]
+
+        public ActionResult<List<Product>> GetAllProducts()
+        {
+            return Ok(Products);
+        }
+
+
+
+    }
+}
---
+++ CRUD/CRUD/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace CRUD.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; } = 0;
+
+        public DateTime DateTime { get; set; }
+    }
+}
